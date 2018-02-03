36507496eaa49007ede35ce1b6c74887444b89c0
simenghe <simengschool@gmail.com>
2018-02-02 22:42:47 -0500

    Add project files.

---
+++ HealthyBot/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace HealthyBot
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+        static Utilities()
+        {
+            string json = File.ReadAllText("Systemlan/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key))
+            {
+                return alerts[key];
+            }
+            return "";
+        }
+    }
+}
